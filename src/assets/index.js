fb9b0ad983d28f9fd9d209eefb2de4334cae53f1
Muhammad-Hassan31144 <31144@students.riphah.edu.pk>
2023-10-12 11:48:22 +0500

    Modified Files

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,34 +1,15 @@
 import logo from "./logo.svg";
-import backend from "./backend.png";
-import creator from "./creator.png";
-import mobile from "./mobile.png";
-import web from "./web.png";
+
 import github from "./github.png";
 import menu from "./menu.svg";
 import close from "./close.svg";
 
-import css from "./tech/css.png";
-import docker from "./tech/docker.png";
-import figma from "./tech/figma.png";
-import git from "./tech/git.png";
-import html from "./tech/html.png";
-import javascript from "./tech/javascript.png";
-import mongodb from "./tech/mongodb.png";
-import nodejs from "./tech/nodejs.png";
-import reactjs from "./tech/reactjs.png";
-import redux from "./tech/redux.png";
-import tailwind from "./tech/tailwind.png";
-import typescript from "./tech/typescript.png";
-import threejs from "./tech/threejs.svg";
 
 import dress from "./company/dress.png";
 import iportfolio from "./company/iportfolio.png";
 import iportrait from "./company/iportrait.png";
 import iwedding from "./company/iwedding.png";
 
-import carrent from "./carrent.png";
-import jobit from "./jobit.png";
-import tripguide from "./tripguide.png";
 
 import fashion from "./exp/fashion.jpg";
 import portfolio from "./exp/portfolio.jpg";
@@ -66,33 +47,13 @@ import wed6 from "./wed/wed6.jpg";
 
 export {
   logo,
-  backend,
-  creator,
-  mobile,
-  web,
   github,
   menu,
   close,
-  css,
-  docker,
-  figma,
-  git,
-  html,
-  javascript,
-  mongodb,
-  nodejs,
-  reactjs,
-  redux,
-  tailwind,
-  typescript,
-  threejs,
   iwedding,
   iportfolio,
   iportrait,
   dress,
-  carrent,
-  jobit,
-  tripguide,
   fashion,
   portfolio,
   portrait,
